5383adac171fd842b4d1083af0da84c6c8f9d772
Maksim Dorogoi <monarch2013@mail.ru>
2024-12-01 19:00:35 +1100

    Спрайт для иконок/компонент Icon и проверка их

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { MyExampleToStart } from './components/MyExampleToStart';
+import { Icon } from './components/Icon';
 
 
 function App() {
   
 
   return (
-    <>
+    <div style={{backgroundColor:'khaki'}}>
       <MyExampleToStart title='MyExampleToStart'/>
-    </>
+      
+      <Icon iconId={"zendesk"}/>
+    
+    </div>
   )
 }
 
---
+++ src/components/Icon.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import SPRITE from "../assets/images/SPRITE.svg";
+
+type IconPropsType = {
+  iconId: string;
+  width?: string;
+  height?: string;
+  viewBox?: string;
+  className?: string;
+};
+
+export class Icon extends React.Component<IconPropsType> {
+  render() {
+    const { iconId, width, height, viewBox, className } = this.props;
+
+    return (
+      <svg
+        width={width}
+        height={height}
+        viewBox={viewBox}
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className={className}
+      >
+        <use xlinkHref={`${SPRITE}#${iconId}`} />
+      </svg>
+    );
+  }
+}
