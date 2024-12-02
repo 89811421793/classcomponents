fd91d6c969b64ed345ca10ee65090733c1c05709
Maksim Dorogoi <monarch2013@mail.ru>
2024-12-02 13:52:01 +1100

    theme.ts от Material UI (MUI theme) и проверка ее

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import "./reset.css";
+import { ThemeProvider, Typography } from "@mui/material";
+import theme from "./mui/theme";
 
 function App() {
-  
-    return (
+  return (
     <div>
-<h1>dfd</h1>
-      
-    
-    
+      <ThemeProvider theme={theme}>
+        <Typography variant="h1" sx={{ color: theme.palette.accent.main }}>
+          Заголовок с использованием акцентного цвета
+        </Typography>
+        <Typography  variant="body1" sx={{ fontFamily: 'Montserrat, sans-serif', fontWeight: 700 }}>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique, omnis in mollitia soluta voluptates accusantium modi deleniti obcaecati earum quia nam nulla eveniet deserunt dignissimos! Amet, recusandae minima. Vero, aliquid?
+        Officiis aspernatur nulla reiciendis nisi ex unde veritatis, illo laudantium iste, nam dicta quia architecto voluptas temporibus repellat vero doloribus expedita maxime, odit fugiat eaque! Error minima laborum sunt officiis!
+        Eligendi placeat accusamus ea obcaecati laboriosam ducimus non odit tempore? Veritatis deleniti ea pariatur porro consequatur animi saepe veniam nobis ducimus, commodi eveniet cum eaque ut? Nam soluta esse nulla.
+        Architecto quaerat repellat saepe placeat, velit fugiat? Incidunt repellat illo, animi ut quam, repudiandae eaque temporibus distinctio doloribus voluptatibus odio odit quae assumenda natus ipsum velit labore asperiores aspernatur dolor.</p>
+        </Typography>
+      </ThemeProvider>
+     
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/mui/theme.ts
@@ -0,0 +1,54 @@
+import { createTheme, PaletteOptions } from '@mui/material/styles';
+
+// Расширяем PaletteOptions
+declare module '@mui/material/styles' {
+    interface Palette {
+        accent: {
+            main: string;
+        };
+    }
+    interface PaletteOptions {
+        accent?: {
+            main: string;
+        };
+    }
+}
+
+// Создаем тему, используя ThemeOptions
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#111214',
+        },
+        secondary: {
+            main: '#A7AAB4',
+        },
+        accent: { // Теперь это допустимо
+            main: '#FE390C',
+        },
+        text: {
+            primary: '#111214',
+            secondary: '#A9A9AA',
+        },
+        background: {
+            default: '#242424', // Темный фон
+            paper: '#ffffff', // Светлый фон
+        },
+    } as PaletteOptions, // Указываем кастомный тип для палитры
+    
+    typography: {
+        fontFamily: 'Anton, Montserrat, Arial, sans-serif',
+        h1: {
+            fontSize: '3.2em',
+            lineHeight: 1.1,
+        },
+        button: {
+            fontWeight: 500,
+        },
+    },
+    shape: {
+        borderRadius: 8,
+    },
+});
+
+export default theme;
