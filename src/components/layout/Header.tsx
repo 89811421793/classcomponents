06d715326a8412fc96d9f894428970b8d8fc840a
Maksim Dorogoi <monarch2013@mail.ru>
2024-12-02 17:25:39 +1100

    черновой вариант Header (Container взят из MUI)/ React.CSSProperties в инлайнах (временно)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Icon } from '../Icon';
+import { Icon } from '../Icon'; // Убедитесь, что этот компонент работает
 import { Menu } from '../menu/Menu';
-import { Button } from '@mui/material'; // Импортируем кнопку из MUI
+import { Button, Container } from '@mui/material';
 
 const items = ["home", "about", "service", "portfolio"];
 
@@ -9,15 +9,17 @@ export class Header extends React.Component {
     render() {
         return (
             <header style={headerStyle}>
-                <div style={logoStyle}>
-                    <Icon iconId='foxlogo' />
-                </div>
-                <Menu menuItems={items} />
-                <div style={buttonContainerStyle}>
-                    <Button variant="contained" color="primary">
-                        Contact
-                    </Button>
-                </div>
+                <Container style={containerStyle}>
+                    <div style={logoStyle}>
+                        <Icon iconId='foxlogo' />
+                    </div>
+                    <Menu menuItems={items} />
+                    <div style={buttonContainerStyle}>
+                        <Button variant="contained" color="primary">
+                            Contact
+                        </Button>
+                    </div>
+                </Container>
             </header>
         );
     }
@@ -25,18 +27,25 @@ export class Header extends React.Component {
 
 // Стили для шапки
 const headerStyle: React.CSSProperties = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
     padding: '10px 20px',
     backgroundColor: '#242424', // Используем фон из темы
     color: '#FFFFFF', // Цвет текста
     width: '100%',
 };
 
+// Стили для контейнера
+const containerStyle: React.CSSProperties = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center', // Выровнять по вертикали
+    height: '60px', // Задайте фиксированную высоту для выравнивания
+};
+
 // Стили для логотипа
 const logoStyle: React.CSSProperties = {
     flex: '0 0 auto',
+    width: '50px', // Задайте ширину логотипа
+    height: '50px', // Задайте высоту логотипа
 };
 
 // Стили для контейнера кнопки
