2a8b771be9ebdd54ec4f054ff5425a8ca4eed890
Maksim Dorogoi <monarch2013@mail.ru>
2024-12-08 13:58:47 +1100

    заголовок и breadcrumbs для AboutTop

--- src/pages/aboutpage/sections/AboutTop.tsx
+++ src/pages/aboutpage/sections/AboutTop.tsx
@@ -1,49 +1,96 @@
-import React, { Component } from 'react';
-import pagetopBg from '../../../assets/images/pagetop_bg.jpg';
-import aboutLetters from '../../../assets/images/About_letters.png';
+import React, { Component } from "react";
+import { Typography, Breadcrumbs, Link } from "@mui/material";
+import pagetopBg from "../../../assets/images/pagetop_bg.jpg";
+import aboutLetters from "../../../assets/images/About_letters.png";
 
-type AboutTopProps = {}
+type AboutTopProps = {};
 
-type AboutTopState = {}
+type AboutTopState = {};
 
 export class AboutTop extends Component<AboutTopProps, AboutTopState> {
-    render() {
-        const sectionStyle: React.CSSProperties = {
-            minHeight: '80vh',
-            backgroundImage: `url(${pagetopBg})`, // Устанавливаем фоновое изображение
-            backgroundSize: 'cover', // 'cover' для фона
-            backgroundPosition: 'center', // Центрируем фон
-            backgroundRepeat: 'no-repeat', // Не повторяем фон
-            position: 'relative', // Для позиционирования текста
-            display: 'flex', // Для центрирования текста
-            alignItems: 'center', // Вертикальное центрирование
-            justifyContent: 'center', // Горизонтальное центрирование
-            color: 'white', // Цвет текста
-            textAlign: 'center', // Выравнивание текста
-        };
+  handleAboutClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault(); // Предотвращаем стандартное поведение ссылки
+  };
 
-        const overlayStyle: React.CSSProperties = {
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            backgroundImage: `url(${aboutLetters})`, // Устанавливаем второе изображение
-            backgroundSize: 'contain', // 'contain' для текста
-            backgroundPosition: 'center', // Центрируем текст
-            backgroundRepeat: 'no-repeat', // Не повторяем текст
-            opacity: 0.8, // Устанавливаем прозрачность для текста
-            display: 'flex', // Для центрирования текста
-            alignItems: 'center', // Вертикальное центрирование
-            justifyContent: 'center', // Горизонтальное центрирование
-        };
+  render() {
+    const sectionStyle: React.CSSProperties = {
+      minHeight: "80vh",
+      backgroundImage: `url(${pagetopBg})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+      position: "relative",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      color: "white",
+      textAlign: "center",
+    };
 
-        return (
-            <section style={sectionStyle}>
-                <div style={overlayStyle}>
-               
-                </div>
-            </section>
-        );
-    }
+    const overlayStyle: React.CSSProperties = {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      backgroundImage: `url(${aboutLetters})`,
+      backgroundSize: "contain",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+      opacity: 0.8,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column", 
+    };
+
+    return (
+      <section style={sectionStyle}>
+        <div style={overlayStyle}>
+          <Typography
+            variant="h1"
+            sx={{
+              color: (theme) => theme.palette.background.paper,
+              textTransform: "uppercase",
+              fontFamily: "Anton",
+              fontSize: "130px",
+              fontWeight: 400,
+            }}
+          >
+            About
+          </Typography>
+          <Breadcrumbs
+            separator={<span style={{ margin: "0 1px" }}>/</span>}
+            aria-label="breadcrumb"
+          >
+            <Link
+              href="#"
+              sx={{
+                color: (theme) => theme.palette.text.secondary,
+                fontFamily: "Montserrat",
+                fontSize: "16px",
+                fontWeight: 700,
+                textDecoration: "none",
+              }}
+            >
+              Home
+            </Link>
+            <Link
+              href="#"
+              sx={{
+                color: (theme) => theme.palette.background.paper,
+                fontFamily: "Montserrat",
+                fontSize: "16px",
+                fontWeight: 700,
+                textDecoration: "none",
+              }}
+              onClick={this.handleAboutClick}
+            >
+              About
+            </Link>
+          </Breadcrumbs>
+        </div>
+      </section>
+    );
+  }
 }
