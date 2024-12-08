105b8531cf20ee04ce106674c4a36a45ad7c2163
Maksim Dorogoi <monarch2013@mail.ru>
2024-12-08 14:36:48 +1100

    PortfolioTop (breadcrumbs and title)

--- src/pages/portfoliopage/sections/PortfolioTop.tsx
+++ src/pages/portfoliopage/sections/PortfolioTop.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Typography, Breadcrumbs, Link } from "@mui/material";
 import pagetopBg from '../../../assets/images/pagetop_bg.jpg';
 import portfolioLetters from '../../../assets/images/Portfolio_letters.png';
 
@@ -7,44 +8,90 @@ type PortfolioTopProps = {}
 type PortfolioTopState = {}
 
 export class PortfolioTop extends Component<PortfolioTopProps, PortfolioTopState> {
+
+    handlePortfolioClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+        event.preventDefault(); // Предотвращаем стандартное поведение ссылки
+    };
+
     render() {
         const sectionStyle: React.CSSProperties = {
-            minHeight: '80vh',
-            backgroundImage: `url(${pagetopBg})`, // Устанавливаем фоновое изображение
-            backgroundSize: 'cover', // 'cover' для фона
-            backgroundPosition: 'center', // Центрируем фон
-            backgroundRepeat: 'no-repeat', // Не повторяем фон
-            position: 'relative', // Для позиционирования текста
-            display: 'flex', // Для центрирования текста
-            alignItems: 'center', // Вертикальное центрирование
-            justifyContent: 'center', // Горизонтальное центрирование
-            color: 'white', // Цвет текста
-            textAlign: 'center', // Выравнивание текста
+            minHeight: "80vh",
+            backgroundImage: `url(${pagetopBg})`, 
+            backgroundSize: "cover", 
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat", 
+            position: "relative", 
+            display: "flex", 
+            alignItems: "center", 
+            justifyContent: "center", 
+            color: "white", 
+            textAlign: "center", 
         };
 
         const overlayStyle: React.CSSProperties = {
-            position: 'absolute',
+            position: "absolute",
             top: 0,
             left: 0,
             right: 0,
             bottom: 0,
-            backgroundImage: `url(${portfolioLetters})`, // Устанавливаем второе изображение
-            backgroundSize: 'contain', // 'contain' для текста
-            backgroundPosition: 'center', // Центрируем текст
-            backgroundRepeat: 'no-repeat', // Не повторяем текст
-            opacity: 0.8, // Устанавливаем прозрачность для текста
-            display: 'flex', // Для центрирования текста
-            alignItems: 'center', // Вертикальное центрирование
-            justifyContent: 'center', // Горизонтальное центрирование
+            backgroundImage: `url(${portfolioLetters})`, 
+            backgroundSize: "contain", 
+            backgroundPosition: "center", 
+            backgroundRepeat: "no-repeat", 
+            opacity: 0.8, 
+            display: "flex",
+            flexDirection: "column", 
+            alignItems: "center", 
+            justifyContent: "center", 
         };
 
         return (
             <section style={sectionStyle}>
                 <div style={overlayStyle}>
-               
+                    <Typography
+                        variant="h1"
+                        sx={{
+                            color: (theme) => theme.palette.background.paper,
+                            textTransform: "uppercase",
+                            fontFamily: "Anton",
+                            fontSize: "130px",
+                            fontWeight: 400,
+                        }}
+                    >
+                        Portfolio
+                    </Typography>
+                    <Breadcrumbs
+                        separator={<span style={{ margin: "0 1px" }}>/</span>}
+                        aria-label="breadcrumb"
+                    >
+                        <Link
+                            href="#"
+                            sx={{
+                                color: (theme) => theme.palette.text.secondary,
+                                fontFamily: "Montserrat",
+                                fontSize: "16px",
+                                fontWeight: 700,
+                                textDecoration: "none",
+                            }}
+                        >
+                            Home
+                        </Link>
+                        <Link
+                            href="#"
+                            sx={{
+                                color: (theme) => theme.palette.background.paper,
+                                fontFamily: "Montserrat",
+                                fontSize: "16px",
+                                fontWeight: 700,
+                                textDecoration: "none",
+                            }}
+                            onClick={this.handlePortfolioClick}
+                        >
+                            Portfolio
+                        </Link>
+                    </Breadcrumbs>
                 </div>
             </section>
         );
     }
 }
-
