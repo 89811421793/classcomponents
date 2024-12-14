e18fc8b50b6f7032e7de39c9df2966c894058770
Maksim Dorogoi <monarch2013@mail.ru>
2024-12-14 13:02:48 +1100

    продолжение стилизации

--- src/pages/aboutpage/sections/AboutMeSection.tsx
+++ src/pages/aboutpage/sections/AboutMeSection.tsx
@@ -1,15 +1,47 @@
 import React from "react";
-import { Container, Typography, Box } from "@mui/material";
+import { Container as MuiContainer, Box, styled } from "@mui/material";
 import SectionTitle from "../../../components/SectionTitle";
+import designAboutImage from "../../../assets/images/design_about.jpg"; // Правильный импорт изображения
 
-export class AboutMeSection extends React.Component {
+export type AboutMeSectionProps = {};
+
+export class AboutMeSection extends React.Component<AboutMeSectionProps> {
   render() {
     return (
-      <section style={{minHeight:'100vh'}}>
-        <Container sx={{minHeight:'70vh', border:'1px solid',marginTop:'80px'}}>
+      <section style={{ minHeight: '100vh' }}>
+        <Container>
+          <LeftBlock>
+            <img src={designAboutImage} alt="About Me" width={485} height={667} />
+          </LeftBlock>
+          <RightBlock>
+       
           <SectionTitle index={1} title={"About Me"} />
+         
+          </RightBlock>
         </Container>
+       
       </section>
     );
   }
 }
+
+const Container = styled(MuiContainer)({
+    minHeight: '70vh',
+    display: 'flex',
+    marginTop: '80px',
+  });
+
+const LeftBlock = styled(Box)({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    border: '1px solid crimson',
+    width: '50%', 
+    height: '100%' 
+  });
+  
+  const RightBlock = styled(Box)({
+    border: '1px solid crimson',
+    width: '50%', 
+    height: '100%'
+  });
