d1d96c5bdcfa0cc38508dfba72417bad98adb208
Maksim Dorogoi <monarch2013@mail.ru>
2024-12-14 17:22:36 +1100

    Переиспользуемый SkillBar компонент (аналог progress bar)

---
+++ src/components/SkillBar.tsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { Box, Typography } from "@mui/material";
+
+type SkillBarProps = {
+  skillName: string;
+  percentage: number;
+};
+
+export class SkillBar extends Component<SkillBarProps> {
+  render() {
+    const { skillName, percentage } = this.props;
+
+    return (
+      <Box sx={{ width: "558px", mb: 2 }}>
+        <Typography variant="body1" sx={{ mb: 1, display: "flex", justifyContent: "space-between" }}>
+          {skillName} <span>{percentage}%</span>
+        </Typography>
+        <Box
+          sx={{
+            width: "100%",
+            height: "24px",
+            backgroundColor: "#DDDEE0",
+            borderRadius: "4px",
+            overflow: "hidden",
+            position: "relative",
+          }}
+        >
+          <Box
+            sx={{
+              width: `${percentage}%`,
+              height: "100%",
+              backgroundColor: "#FE390C",
+              position: "absolute",
+              top: 0,
+              left: 0,
+            }}
+          />
+        </Box>
+      </Box>
+    );
+  }
+}
--- src/pages/aboutpage/sections/MySkills.tsx
+++ src/pages/aboutpage/sections/MySkills.tsx
@@ -1,19 +1,80 @@
 import { Component } from "react";
-import { Container, Box, Typography } from "@mui/material";
+import { Container, Box, Typography, Button } from "@mui/material";
 import SectionTitle from "../../../components/SectionTitle";
+import { SkillBar } from "../../../components/SkillBar";
 
-export class MySKills extends Component {
+type MySkillsProps = {};
+
+type MySkillsState = {};
+
+export class MySKills extends Component<MySkillsProps, MySkillsState> {
   render() {
     return (
-      <section style={{ minHeight: "80vh", padding:'80px 150px', backgroundColor: "#F7F7F7" }}>
-        <Container sx={{minHeight:'80vh', display:'flex', justifyContent:'space-between', border:'2px ridge navy'}}>
-            <Box sx={{display:'flex', flexDirection:'column', justifyContent:'flex-start', alignItems:'flex-start', border:'2px dotted forestgreen'}}>
-                <SectionTitle index={2} title={'My Skills'}/>
-            </Box>
+      <section
+        style={{
+          minHeight: "50vh",
+          padding: "80px 150px",
+          backgroundColor: "#F7F7F7",
+        }}
+      >
+        <Container
+          sx={{
+            minHeight: "50vh",
+            display: "flex",
+            justifyContent: "space-between",
+            border: "2px ridge navy",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+              justifyContent:'space-between',
+              border: "2px dotted forestgreen",
+            }}
+          >
+            <SectionTitle index={2} title={"My Skills"} />
+            <Typography
+              sx={{
+                color: "#A9A9AA",
+                fontFamily: "Montserrat",
+                fontSize: "14px",
+                fontWeight: 500,
+                lineHeight: "32px",
+                maxWidth:'485px'
+              }}
+            >
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+            </Typography>
+            <Button
+              sx={{
+                padding: "16px 24px",
+                color: "white",
+                textTransform: "uppercase",
+                fontFamily: "Montserrat",
+                fontSize: "12px",
+                fontWeight: 600,
+                backgroundColor: "#FE390C", 
+                borderRadius:'3px'
+              }}
+            >
+              Download CV
+            </Button>
+          </Box>
 
-            <Box sx={{display:'flex', flexDirection:'column', border:'2px dotted forestgreen'}}>
-                правый блок
-            </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              border: "2px dotted forestgreen",
+            }}
+          >
+             <SkillBar skillName="UI/UX Design" percentage={90} />
+            <SkillBar skillName="Front End Developer" percentage={95} />
+            <SkillBar skillName="Graphic Design" percentage={90} />
+            <SkillBar skillName="Product Design" percentage={85} />
+          </Box>
         </Container>
       </section>
     );
