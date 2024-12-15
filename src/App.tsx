2e5343aaacee8122882784aab7b12b96c77ef2c2
Maksim Dorogoi <monarch2013@mail.ru>
2024-12-15 18:15:35 +1100

    Подготовка к подключению маршрутов (роуты)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { About } from "./pages/aboutpage/About";
+import { Home } from "./pages/homepage/Home";
+// import { About } from "./pages/aboutpage/About";
+// import { Contacts } from "./pages/contactspage/Contacts";
+// import { Portfolio } from "./pages/portfoliopage/Portfolio";
+// import { Service } from "./pages/servicepage/Service";
 import "./reset.css";
 
 function App() {
   return (
     <div>
-   <About/>
-
+   <Home/>
+   {/* <About/>
+   <Service/>
+   <Portfolio/>
+   <Contacts/> */}
     </div>
   );
 }
