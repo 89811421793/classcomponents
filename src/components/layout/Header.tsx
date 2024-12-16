12e21d4a3271d0667231c0936c07418338264bf6
Maksim Dorogoi <monarch2013@mail.ru>
2024-12-16 15:38:14 +1100

    position: fixed - для Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -20,6 +20,11 @@ export class Header extends React.Component {
         sx={{
           padding: "10px 20px",
           backgroundColor: (theme) => theme.palette.background.default,
+          position: "fixed", 
+          top: 0, 
+          left: 0,
+          right: 0, 
+          zIndex: 1000, 
         }}
       >
         <Container
